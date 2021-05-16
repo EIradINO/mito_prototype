fc0425b621c341afd81a84176dee6406ab910b79
EIradINO <eisukeinoue0107@gmail.com>
2021-05-16 22:42:15 +0900

    save scraping data

--- routes/contents.js
+++ routes/contents.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const elasticlunr = require('elasticlunr')
+require('./lunr.stemmer.support.js')(elasticlunr);
+require('./lunr.jp.js')(elasticlunr);
 const db = require('../models');
 const router = express.Router();
 
@@ -9,6 +12,39 @@ router.get('/', (req, res, next) => {
       contents: cts,
     }
     res.render('contents/index', data)
+  })
+})
+
+router.get('/search', (req, res, next) => {
+  const index = elasticlunr(function () {
+    this.use(elasticlunr.jp);
+    this.addField('title');
+    this.addField('description');
+    this.setRef('id');
+  });
+  db.Content.findAll().then(cts => {
+    var i = 0;
+    for (content of cts) {
+      i++;
+      index.addDoc({
+        "id": i,
+        "title": content.title,
+        "description": content.description,
+      })
+    }
+
+    wordlist = ["空", "青い", "の", "気"]
+    for (word of wordlist) {
+      const r = index.search(word, {
+        fields: {
+          title: {boost: 1},
+          description: {boost: 1},
+        },
+        expand: true,
+      });
+      console.log(r);
+    }
+    res.render('contents/search', { title: 'Search' })
   })
 })
 
